738c09d2e70a0c7c775f50eab07986d0bbb82199
wagd96 <wagd96@gmail.com>
2019-02-19 15:46:20 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDhCDTVVLnVF3tR1EWqA-CQoqArKZPM-ME",
+    authDomain: "indranews-a2dca.firebaseapp.com",
+    databaseURL: "https://indranews-a2dca.firebaseio.com",
+    projectId: "indranews-a2dca",
+    storageBucket: "indranews-a2dca.appspot.com",
+    messagingSenderId: "764495305563"
+  }
+};
